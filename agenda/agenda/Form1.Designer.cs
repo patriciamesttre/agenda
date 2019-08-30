aca6b2f77cd973e8cbed83291eb0be96578a4a65
patriciamesttre <patriciamesttre@hotmail.com>
2019-08-29 21:17:19 -0300

    Agenda

---
+++ agenda/agenda/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOVO NOME";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CADASTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(445, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "EXCLUIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "ALTERAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(445, 153);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "CONSULTAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(23, 201);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(545, 238);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CADASTRO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ agenda/agenda/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //BOTÃO CADASTRAR
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            String connect = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\patricia.mmribeiro\Downloads\teste.accdb";
+            conn.ConnectionString = connect;
+            OleDbCommand cmd = new OleDbCommand("INSERT INTO tabela1(nome) Values ('" +textBox1.Text+"')");
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Dado cadastrado");
+            conn.Close();
+        }
+
+        //botao excluir
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            String connect = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\patricia.mmribeiro\Downloads\teste.accdb";
+            conn.ConnectionString = connect;
+            OleDbCommand cmd = new OleDbCommand("DELETE FROM tabela1 WHERE nome = ('" +textBox1.Text+"')");
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Dado apagado");
+            conn.Close();
+
+
+        }
+
+        //botao alterar
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            String connect = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\patricia.mmribeiro\Downloads\teste.accdb";
+            conn.ConnectionString = connect;
+            OleDbCommand cmd = new OleDbCommand("UPDATE tabela1 Set nome = ('" +textBox2.Text+ "') WHERE nome = ('" + textBox1.Text + "')");
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Dado atualizado");
+            conn.Close();
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            String connect = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\patricia.mmribeiro\Downloads\teste.accdb";
+            conn.ConnectionString = connect;
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM tabela1 WHERE nome Like ('" + textBox1.Text + "%' ORDER BY nome ASC");
+            cmd.Connection = conn;
+
+            conn.Open();
+            OleDbDataReader aReader = cmd.ExecuteReader();
+            textBox3.Text = "Os valores retornados da tabela são : ";
+            while (aReader.Read())
+            {
+                textBox3.Text += Environment.NewLine + (aReader.GetString(0));
+            }
+            conn.Close();
+        }
+    }
+}
